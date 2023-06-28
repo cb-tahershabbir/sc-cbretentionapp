dab589bc5ac2c29d71c35340ebda7ffedd5b0542
Nivedha Venkatesh <nivedha.venkat@chargebee.com>
2023-06-28 16:13:14 +0530

    test

--- public/usecase_widgets/other_page.js
+++ public/usecase_widgets/other_page.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Retrieve the parameter values from the URL
     const urlParams = new URLSearchParams(window.location.search);
+    const sitedomain = urlParams.get('site_domain');
     const subscriptionId = urlParams.get('subscription_id');
     const customerId = urlParams.get('customer_id');
-    const appId = urlParams.get('api_key');
+    const apikey = urlParams.get('api_key');
   
     // Update the value in the HTML elements
+    document.getElementById('site_domain').textContent = sitedomain;
     document.getElementById('subscription_id').textContent = subscriptionId;
     document.getElementById('customer_id').textContent = customerId;
-    document.getElementById('api_key').textContent = appId;
+    document.getElementById('api_key').textContent = apikey;
   });
   
